f836f2dd3f3a5437951c1da4b67738f9aa37fb01
Allan da Silva Ferreira <allan.ferreira@mouts.info>
2022-04-24 16:06:52 -0300

    trata possíveis erros de retorno da api na criação da order

--- lib/services/order_service.dart
+++ lib/services/order_service.dart
@@ -1,6 +1,31 @@
 import 'package:abctechapp/model/order.dart';
 import 'package:abctechapp/model/order_created.dart';
+import 'package:abctechapp/provider/order_provider.dart';
+import 'package:flutter/rendering.dart';
+import 'package:get/get.dart';
 
 abstract class OrderServiceInterface {
   Future<OrderCreated> createOrder(Order order);
+}
+
+class OrderService extends GetxService implements OrderServiceInterface {
+  final OrderProviderInterface _orderProvider;
+
+  OrderService(this._orderProvider);
+
+  @override
+  Future<OrderCreated> createOrder(Order order) async {
+    Response response = await _orderProvider.postOrder(order);
+    try {
+      if (response.hasError) {
+        return Future.sync(() => OrderCreated(
+            success: false, message: response.statusText.toString()));
+      }
+
+      return Future.sync(() => OrderCreated(success: true, message: ''));
+    } catch (e) {
+      e.printError();
+      return Future.error(ErrorDescription("Erro não esperado"));
+    }
+  }
 }
